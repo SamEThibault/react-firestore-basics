d3cdeced1f56893d59345f96808712529662c3e1
Samuel Emard-Thibault <92962712+SamEThibault@users.noreply.github.com>
2022-03-19 01:18:44 -0400

    updated project structure

--- src/App.js
+++ src/App.js
@@ -1,52 +1,28 @@
-import './App.css';
-import { Dropdown, form } from 'react-bootstrap';
+import './Styles/App.css';
 import { React, useState } from 'react';
-import userQuery from './Query.js';
+import Home from './Views/Home.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ViewDB from "./Views/ViewDB";
 
-function App() {
 
-  // used to call the query function from Query.js, specified using dropdown menu
-  const [operation, setOperation] = useState("CREATE");
 
-  // call the userQuery using the operation and the user's input field onSubmit
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const name = event.target[0].value;
-    userQuery(operation, name);
+export default function App() {
+
+  const [view, setView] = useState("Home");
+
+  // use redux to make the view state global to use setView in other components
+   function handleView(viewToSet)
+  {
+    setView(viewToSet);
   }
 
   return (
-    <div className="App">
-
-      <h1 className="Header">WELCOME TO THE FIRESTORE TEST PROGRAM</h1>
-
-      <div className="Query-text-box">
-        <h6> The query operation is: {operation}, click on the query type button to change the operation! </h6>
-      </div>
-
-      <form className="Form" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label for="exampleInputEmail1">What would you like to do?</label>
-          <input type="form" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Query Here" />
-        </div>
-        <small>This will send a request to the firestore "names" collection </small>
-        <h2></h2>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-
-      <Dropdown className="Dropdown">
-        <Dropdown.Toggle id="dropdown-basic">
-          Query Type
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={e => setOperation("CREATE")}>CREATE</Dropdown.Item>
-          <Dropdown.Item onClick={e => setOperation("DELETE")}>DELETE</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+    <div>
+    {view === "Home" ? <Home/> : ""}
+    {view === "ViewDB" ? <ViewDB/> : ""}
     </div>
+ 
+
   );
 }
 
-export default App;
